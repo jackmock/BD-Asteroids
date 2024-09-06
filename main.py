baf30f31437afbc7551f55af63d316c46e2aa91c
Jack <jack.r.mock@gmail.com>
2024-09-05 20:52:30 -0500

    created circleshape.py, which houses game objects. Created player.py which handles the player object. Updated main.py to draw player object every frame.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import pygame
-
-import constants
+import pygame, constants, player
 
 def main():
     pygame.init()
@@ -11,15 +9,22 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-
+    
+    player_obj = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2, constants.PLAYER_RADIUS)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill((0, 0, 0))
+
+        player_obj.draw(screen)
+        
         pygame.display.flip()
-        dt = (clock.tick(60))/1000
+        
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
